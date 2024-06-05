27a19a4fc619e3cb4c998d702b1472d2b75daec6
ananyagoel77 <tiyagoel078@gmail.com>
2024-06-05 10:08:28 +0530

    Create Ethereumbeginner.sol

---
+++ Ethereumbeginner.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
+contract Token {
+
+    // public variables here
+    string public tokenName;
+    string public tokenAbbrv;
+    uint public totalSupply;
+
+    // mapping variable here
+    mapping(address => uint) public balances;
+        // Constructor to initialize the token details
+        constructor(string memory _name, string memory _abbrv, uint _initialSupply) {
+            tokenName = _name;
+            tokenAbbrv = _abbrv;
+            totalSupply = _initialSupply;
+            // Assign initial supply to the contract deployer
+            balances[msg.sender] = _initialSupply;
+    }
+
+    // mint function
+        function mint(address _add, uint _amount) public {
+        totalSupply += _amount;
+        balances[_add] += _amount;
+    }
+
+    // burn function
+    function burn(address _add, uint _amount) public {
+        if(balances[_add] >= _amount){
+            totalSupply -= _amount;
+            balances[_add] -= _amount;
+            }
+        }
+}
